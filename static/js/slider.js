b4bb48867a440131165769957f6b65b20d42fcb3
Krzysztof Wilczyński <kwilczyn89@icloud.com>
2024-07-09 14:50:37 +0200

    Add a slider for opinions

--- static/js/slider.js
+++ static/js/slider.js
@@ -1,36 +1,44 @@
 let certificatesSection = document.querySelector("#certificates");
-let mediaRoot = "static/media/images/"
+let opinionsSection = document.querySelector("#opinions");
+let mediaRoot = "static/media/images/";
 
+create_slider(certificatesSection, read_images("certificates"));
+create_slider(opinionsSection, read_images("opinions"));
 
-create_slider(certificatesSection, read_images());
-
-
-function read_images() {
-    let images = [`${mediaRoot}cert1.jpeg`,`${mediaRoot}cert2.jpeg`,`${mediaRoot}cert3.jpeg`];
-    return images;
+function read_images(dir) {
+  function add_media_root(file_name) {
+    return `${mediaRoot}${dir}/${file_name}`;
+  }
+  let images = null;
+  if (dir === "certificates") {
+    images = ["cert1.jpeg", "cert2.jpeg", "cert3.jpeg"];
+  } else {
+    images = ["op1.png", "op2.png"];
+  }
+  return images.map(add_media_root);
 }
 
-function create_slider(parent, images){
-    sliderContainer = document.createElement("div");
-    sliderContainer.classList.add("slider");
-    parent.appendChild(sliderContainer);
-    for ( let i=0; i<images.length; i++){
-        let image = document.createElement("img");
-        image.setAttribute("src", images[i]);
-        sliderContainer.appendChild(image);        
-    }
-    slideShow(sliderContainer);
-    console.log("polazłem sobie!");
+function create_slider(parent, images) {
+  sliderContainer = document.createElement("div");
+  sliderContainer.classList.add("slider");
+  parent.appendChild(sliderContainer);
+  for (let i = 0; i < images.length; i++) {
+    let image = document.createElement("img");
+    image.setAttribute("src", images[i]);
+    sliderContainer.appendChild(image);
+  }
+  slideShow(sliderContainer);
+  console.log("polazłem sobie!");
 }
 
-function slideShow(slider){
-    images = slider.querySelectorAll("img");
-    let currentImageIndex=0;
+function slideShow(slider) {
+  const images = slider.querySelectorAll("img");
+  let currentImageIndex = 0;
+  images[currentImageIndex].classList.add("active");
+  function showNextImage() {
+    images[currentImageIndex].classList.remove("active");
+    currentImageIndex = (currentImageIndex + 1) % images.length;
     images[currentImageIndex].classList.add("active");
-    function showNextImage(){
-        images[currentImageIndex].classList.remove("active");
-        currentImageIndex = (currentImageIndex + 1) % images.length;
-        images[currentImageIndex].classList.add("active");
-    }
-    setInterval(showNextImage, 3000); 
+  }
+  setInterval(showNextImage, 3000);
 }
