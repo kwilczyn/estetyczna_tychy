99571ee4b20e2db3914de9f4d4a255550d0017c2
Krzysztof Wilczyński <kwilczyn89@icloud.com>
2024-07-24 21:43:40 +0200

    fix: Fix the issue about cutting paths from urls

--- tests/E2E/navigation.test.js
+++ tests/E2E/navigation.test.js
@@ -1,24 +1,24 @@
 import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-  await page.goto("/index.html");
+  await page.goto("./index.html");
   await expect(page).toHaveTitle("Medycyna Estetyczna Tychy");
 });
 
 test("shows nav bar", async ({ page }) => {
-  await page.goto("/index.html");
+  await page.goto("./index.html");
   await expect(page.getByRole("navigation")).toBeInViewport();
 });
 
 test("nav bar disappears after scrolling down", async ({ page }) => {
-  await page.goto("/index.html");
+  await page.goto("./index.html");
 
   await page.mouse.wheel(0, 500);
   await expect(page.getByRole("navigation")).not.toBeInViewport();
 });
 
 test("nav bar appears after scrolling up", async ({ page }) => {
-  await page.goto("/index.html");
+  await page.goto("./index.html");
   await page.mouse.wheel(0, 500);
   await page.mouse.wheel(0, -500);
   await expect(page.getByRole("navigation")).toBeInViewport();
