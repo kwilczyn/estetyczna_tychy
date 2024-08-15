fd54a4a308f9955d5991df4cef4bb021f68f1063
Krzysztof Wilczyński <kwilczyn89@icloud.com>
2024-08-16 00:49:30 +0200

    feat: add an alternative version of a slider for opinions

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,3 +1,5 @@
+import { prepare_element } from "./utils.js";
+
 let certificatesSection = document.querySelector("#certificates");
 let opinionsSection = document.querySelector("#opinions");
 let prologSection = document.querySelector("#prolog");
@@ -44,21 +46,72 @@ function create_slider(parent, images) {
     image.setAttribute("src", images[i]);
     sliderContainer.appendChild(image);
   }
-  slideShow(sliderContainer);
+  slideShow(sliderContainer, "img");
 }
 
-function slideShow(slider) {
-  const images = slider.querySelectorAll("img");
+function slideShow(slider, slidesSelector) {
+  const slides = slider.querySelectorAll(slidesSelector);
   let currentImageIndex = 0;
-  images[currentImageIndex].classList.add("active");
+  slides[currentImageIndex].classList.add("active");
   function showNextImage() {
-    images[currentImageIndex].classList.remove("active");
-    currentImageIndex = (currentImageIndex + 1) % images.length;
-    images[currentImageIndex].classList.add("active");
+    slides[currentImageIndex].classList.remove("active");
+    currentImageIndex = (currentImageIndex + 1) % slides.length;
+    slides[currentImageIndex].classList.add("active");
   }
   setInterval(showNextImage, 3000);
 }
 
+function createOpinionSlider(parent, opinions) {
+  let sliderContainer = document.createElement("div");
+  sliderContainer.classList.add("slider");
+  parent.appendChild(sliderContainer);
+  for (let i = 0; i < opinions.length; i++) {
+    let opinion = prepare_element(sliderContainer, "div", null);
+    opinion.classList.add("opinion");
+    prepare_element(opinion, "span", opinions[i].author);
+    prepare_element(opinion, "span", opinions[i].date);
+    prepare_element(opinion, "span", opinions[i].localization);
+    prepare_element(opinion, "p", opinions[i].content);
+  }
+  slideShow(sliderContainer, ".opinion");
+}
+
 create_slider(prologSection, read_images("prolog"));
 create_slider(certificatesSection, read_images("certificates"));
-create_slider(opinionsSection, read_images("opinions"));
+
+async function getOpinions(zlUrl) {
+  const response = await fetch("https://corsproxy.io/?" + zlUrl);
+  const data = await response.json();
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(data.html, "text/html");
+  const opinionsElements = doc.body.querySelectorAll(".opinion");
+  const opinions = Array();
+  for (let i = 0; i < opinionsElements.length; i++) {
+    const metaData = opinionsElements[i].querySelector(".media-body > .mb-1");
+    opinions.push({
+      author: metaData.querySelector("span:last-of-type").innerText,
+      date: metaData.querySelector("time").innerText,
+      localization: metaData.querySelector(":scope > span:last-of-type")
+        .innerText,
+      content: opinionsElements[i].querySelector(".media-body > p").innerText,
+    });
+  }
+  return opinions;
+}
+
+let allOpinions = Array();
+
+getOpinions(
+  "https://www.znanylekarz.pl/ajax/mobile/doctor-opinions/362941/1?rating-sort=desc"
+)
+  .then((opinions) => {
+    allOpinions = allOpinions.concat(opinions);
+    return getOpinions(
+      "https://www.znanylekarz.pl/ajax/mobile/doctor-opinions/297822/1?rating-sort=desc"
+    );
+  })
+  .then((opinions) => {
+    allOpinions = allOpinions.concat(opinions);
+    allOpinions = allOpinions.sort(() => Math.random() - 0.5);
+    createOpinionSlider(opinionsSection, allOpinions);
+  });
